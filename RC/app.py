453c858f94fbe7cea3dd615c897c426bd56d6179
Wagz28 <benwagstaff365@icloud.com>
2024-10-25 11:36:25 +0100

    8000 port

--- RC/app.py
+++ RC/app.py
@@ -14,4 +14,4 @@ def home():
     return render_template('styling-lists.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=9443, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
